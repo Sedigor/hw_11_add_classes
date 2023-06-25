dc646d1bcd03f15974c61ff67537bdc2c363ebfe
Ihor Siedielnikov <sisemail92@gmail.com>
2023-06-25 14:39:54 +0300

    ab_iteration

--- main.py
+++ main.py
@@ -3,49 +3,60 @@ from datetime import datetime
 
 
 class Field:
-    pass
+    def __init__(self, value=None):
+        self._value = value
+    
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        self._value = value  
+    
         
-
-class Name:
+class Name(Field):
     def __init__(self, name):
         self.value = name
 
 
-class Phone:
+class Phone(Field):
     def __init__(self, phone):
-        self.value = phone
+        super().__init__(phone)
         
     @property
     def value(self):
         return self.value
     
-    @value.setter
+    @Field.value.setter
     def value(self, new_value):
+        if self._is_valid_phone(new_value):
+            self._value = new_value
+        else:
+            raise ValueError("Invalid phone number")
+        
+    def _is_valid_phone(phone):
         try:
-            _ = int(new_value)
-            self.value = new_value
+            _ = int(phone)
+            return True
         except ValueError:
-            return "Wrong type of phone number. Phone number mast have only integers"
+            return False
         
         
-class Birthday:
-    def __init__(self, value):
-        self.day = value
+class Birthday(Field):
+    def __init__(self, birthday):
+        super().__init__(birthday)
     
-    @property
-    def value(self):
-        return self.value
-
-    @value.setter
+    @Field.value.setter
     def value(self, birthday):
         if self._is_valid_birthday(birthday):
             self.value = birthday
         else:
-            raise ValueError("Wrong birthday date format. Try again in format dd/mm/yyyy")
+            raise ValueError("Wrong birthday date format. Try again in format dd.mm.yyyy")
         
     def _is_valid_birthday(self, birthday):
         try:
-            day, month, year = map(int, birthday.split('/'))
+            day, month, year = map(int, birthday.split('.'))
             if day < 1 or day > 31 or month < 1 or month > 12 or len(year) != 4:
                 return False
         except ValueError:
@@ -74,28 +85,39 @@ class Record:
                 p.value = new_phone
                 break
             
-    def days_to_birthday(self, birthday):
-        if birthday:
-            pass
+    def days_to_birthday(self):
+        today = datetime.date.today()
+        user_birthday = self.birthday.value
+        day, month, year = map(int, user_birthday.split('.'))
+        next_birthday = datetime.date(today.year, month, day)
+
+        if next_birthday < today:
+            next_birthday = datetime.date(today.year + 1, month, day)
+
+        days_left = (next_birthday - today).days
+        return days_left
             
 
 class AddressBook(UserDict):
     def __init__(self):
         self.data = {}
+        self.current_index = 0
         
     def add_record(self, record):
         self.data[record.name.value] = record
         
+    def __iter__(self):
+        return self.iterator()
+
+    def __next__(self):
+        if self.current_index >= len(self.entries):
+            raise StopIteration
+
+        entry = self.entries[self.current_index]
+        self.current_index += 1
+        return entry
         
-if __name__ == "__main__":
-    name = Name('Bill')
-    phone = Phone('1234567890')
-    rec = Record(name, phone)
-    ab = AddressBook()
-    ab.add_record(rec)
-    assert isinstance(ab['Bill'], Record)
-    assert isinstance(ab['Bill'].name, Name)
-    assert isinstance(ab['Bill'].phones, list)
-    assert isinstance(ab['Bill'].phones[0], Phone)
-    assert ab['Bill'].phones[0].value == '1234567890'
-    print('All Ok)')
+    def iterator(self, n):
+        
+        
+        
